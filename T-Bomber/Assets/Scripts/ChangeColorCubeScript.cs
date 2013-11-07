d6daf4a57197df6d90a533ff8131f27b8c12a187
ThomasEtcheberry <etcheberry.thomas@gmail.com>
2013-11-07 21:19:09 +0100

    Alpha a rendre version Finale

---
+++ T-Bomber/Assets/Scripts/ChangeColorCubeScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeColorCubeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(Input.GetKeyDown(KeyCode.R))
+		{
+			gameObject.renderer.material.color = Color.red;
+		}
+		
+		if(Input.GetKeyDown(KeyCode.G))
+		{
+			gameObject.renderer.material.color = Color.green;
+		}
+		
+		if(Input.GetKeyDown(KeyCode.B))
+		{
+			gameObject.renderer.material.color = Color.blue;
+		}
+		
+		
+	}
+}
